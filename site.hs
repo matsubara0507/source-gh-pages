67983601ec032ac127777f17dbab9097094ae3c7
MATSUBARA Nobutada <t12307043@gunma-u.ac.jp>
2018-02-20 05:18:00 +0900

    Refactor: change post file path

--- site.hs
+++ site.hs
@@ -1,9 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
-import           Data.Map    (Map, foldMapWithKey)
-import           Data.Monoid (mappend)
-import           Data.Yaml   (decodeFileEither)
+{-# LANGUAGE TupleSections     #-}
+
+import           Control.Monad   (liftM)
+import           Data.List       (sortBy)
+import           Data.Map        (Map, foldMapWithKey)
+import           Data.Ord        (comparing)
+import           Data.Time
+import           Data.Yaml       (decodeFileEither)
 import           Hakyll
-import           Skylighting (pygments, styleToCss)
+import           Skylighting     (pygments, styleToCss)
+import           System.FilePath (takeBaseName, takeDirectory, takeFileName)
 
 main :: IO ()
 main = do
@@ -14,26 +20,27 @@ main = do
     match ("templates/*" .||. "includes/*") $ compile templateBodyCompiler
 
     create ["css/highlight.css"] $ do
-        route   idRoute
+        route idRoute
         compile $ makeItem (compressCss $ styleToCss pygments)
 
     match ("assets/*" .||. "assets/*/*") $ do
-        route   idRoute
+        route idRoute
         compile copyFileCompiler
 
     match "css/*.css" $ do
-        route   idRoute
+        route idRoute
         compile compressCssCompiler
 
-    match "posts/*" $ do
-        route $ setExtension "html"
+    match "posts/*/*" $ do
+        route $
+          gsubRoute "/[0-9]{4}/" ((++ "-") . init) `composeRoutes` setExtension "html"
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/post.html"    postCtx
             >>= loadAndApplyTemplate "templates/default.html" (postCtx `mappend` siteCtx)
             >>= relativizeUrls
 
     match "about.md" $ do
-        route   $ setExtension "html"
+        route $ setExtension "html"
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/page.html" (siteCtx `mappend` defaultContext)
             >>= loadAndApplyTemplate "templates/default.html" (siteCtx `mappend` defaultContext)
@@ -42,7 +49,7 @@ main = do
     create ["archive.html"] $ do
       route idRoute
       compile $ do
-          posts <- recentFirst =<< loadAll "posts/*"
+          posts <- recentFirst' =<< loadAll "posts/*/*"
           let archiveCtx =
                 listField "posts" postCtx (return posts) `mappend`
                 constField "title" "Archives" `mappend`
@@ -57,7 +64,7 @@ main = do
     match "index.html" $ do
       route idRoute
       compile $ do
-        posts <- take 4 <$> (recentFirst =<< loadAll "posts/*")
+        posts <- take 4 <$> (recentFirst' =<< loadAll "posts/*/*")
         let indexCtx =
               listField "posts" postCtx (return posts) `mappend`
               boolField "isIndex" (const True) `mappend`
@@ -72,7 +79,7 @@ main = do
     create ["sitemap.xml"] $ do
         route idRoute
         compile $ do
-          posts <- recentFirst =<< loadAll "posts/*"
+          posts <- recentFirst' =<< loadAll "posts/*/*"
           let
             sitemapCtx =
               listField "entries" (postCtx `mappend` siteCtx) (return posts)
@@ -82,9 +89,33 @@ main = do
 
 postCtx :: Context String
 postCtx =
-  dateField "time" "%Y-%m-%d" `mappend`
-  dateField "date" "%b %-d, %Y" `mappend`
+  dateField' "time" "%Y-%m-%d" `mappend`
+  dateField' "date" "%b %-d, %Y" `mappend`
   defaultContext
+
+dateField' :: String -> String -> Context a
+dateField' key format = field key $ \item -> do
+  time <- getItemUTC' defaultTimeLocale $ itemIdentifier item
+  return $ formatTime defaultTimeLocale format time
+
+getItemUTC' :: MonadMetadata m => TimeLocale -> Identifier -> m UTCTime
+getItemUTC' locale ident =
+  pure $ parseTimeOrError True locale "%Y%m-%d" (yyyy ++ mmdd)
+  where
+    path = toFilePath ident
+    yyyy = takeFileName $ takeDirectory path
+    mmdd = take 5 $ takeBaseName path
+
+chronological' :: MonadMetadata m => [Item a] -> m [Item a]
+chronological' =
+  sortByM $ getItemUTC' defaultTimeLocale . itemIdentifier
+
+recentFirst' :: MonadMetadata m => [Item a] -> m [Item a]
+recentFirst' = liftM reverse . chronological'
+
+sortByM :: (Monad m, Ord k) => (a -> m k) -> [a] -> m [a]
+sortByM f xs =
+  liftM (map fst . sortBy (comparing snd)) $ mapM (\x -> liftM (x,) (f x)) xs
 
 type Config = Map String String
 
