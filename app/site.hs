9d58114975c0df2cdd1c4c9318eb596a1f3e4644
MATSUBARA Nobutada <t12307043@gunma-u.ac.jp>
2018-02-20 19:48:43 +0900

    Feat: tags

--- app/site.hs
+++ app/site.hs
@@ -1,13 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import           Config
-import           Data.List       (sortBy)
-import           Data.Monoid     ((<>))
-import           Data.Ord        (comparing)
+import           Data.List                   (intersperse, sortBy)
+import           Data.Monoid                 ((<>))
+import           Data.Ord                    (comparing)
 import           Data.Time
 import           Hakyll
-import           Skylighting     (pygments, styleToCss)
+import           Skylighting                 (pygments, styleToCss)
 import           System.FilePath
+import           Text.Blaze.Html             (toHtml, toValue, (!))
+import qualified Text.Blaze.Html5            as H
+import qualified Text.Blaze.Html5.Attributes as A
 
 main :: IO ()
 main = do
@@ -28,14 +31,31 @@ main = do
       route idRoute
       compile compressCssCompiler
 
+    tags <- buildTags "posts/*/*" (fromCapture "tags/*.html")
+    let postCtx' = postCtx tags
+
+    tagsRules tags $ \tag pattern -> do
+      route idRoute
+      compile $ do
+        posts <- recentFirst' =<< loadAll pattern
+        let tagCtx = mconcat
+              [ constField "title" ("Posts tagged “" ++ tag ++ "”")
+              , listField "posts" postCtx' (return posts)
+              , defaultContext
+              ]
+        makeItem ""
+          >>= loadAndApplyTemplate "templates/tag.html"     tagCtx
+          >>= loadAndApplyTemplate "templates/default.html" (tagCtx <> siteCtx)
+          >>= relativizeUrls
+
     match "posts/*/*" $ do
       route $ composeRoutes (gsubRoute "/[0-9]{4}/" $ (++ "-") . init)
                             (setExtension "html")
       compile
         $   pandocCompiler
-        >>= loadAndApplyTemplate "templates/post.html" postCtx
+        >>= loadAndApplyTemplate "templates/post.html" postCtx'
         >>= saveSnapshot "content"
-        >>= loadAndApplyTemplate "templates/default.html" (postCtx <> siteCtx)
+        >>= loadAndApplyTemplate "templates/default.html" (postCtx' <> siteCtx)
         >>= relativizeUrls
 
     match "about.md" $ do
@@ -58,7 +78,7 @@ main = do
         posts <- recentFirst' =<< loadAll pattern
         let archiveCtx = mconcat
               [ constField "title" "Archives"
-              , listField "posts" postCtx (return posts)
+              , listField "posts" postCtx' (return posts)
               , paginateContext archive pageNum
               , siteCtx
               , defaultContext
@@ -71,10 +91,12 @@ main = do
     match "index.html" $ do
       route idRoute
       compile $ do
-        posts <- takeRecentFirst' 4 =<< loadAll "posts/*/*"
+        posts    <- takeRecentFirst' 4 =<< loadAll "posts/*/*"
+        tagCloud <- renderTagCloud 80.0 120.0 tags
         let indexCtx = mconcat
-              [ listField "posts" postCtx (return posts)
-              , boolField "isIndex" (const True)
+              [ listField "posts" postCtx' (return posts)
+              , boolField  "isIndex"  (const True)
+              , constField "tagcloud" tagCloud
               , siteCtx
               , defaultContext
               ]
@@ -88,22 +110,23 @@ main = do
       compile $ do
         posts <- recentFirst' =<< loadAll "posts/*/*"
         let sitemapCtx =
-              listField "entries" (postCtx <> siteCtx) (return posts)
+              listField "entries" (postCtx' <> siteCtx) (return posts)
         makeItem [] >>= loadAndApplyTemplate "templates/sitemap.xml" sitemapCtx
 
     create ["feed.xml"] $ do
       route idRoute
       compile $ do
-        let feedCtx = postCtx <> bodyField "description"
+        let feedCtx = postCtx' <> bodyField "description"
         posts <- takeRecentFirst' 10 =<< loadAllSnapshots "posts/**" "content"
         renderAtom (mkFeedConfig config) feedCtx posts
 
-postCtx :: Context String
-postCtx = mconcat
+postCtx :: Tags -> Context String
+postCtx tags = mconcat
   [ dateField' "time"      "%Y-%m-%d"
   , dateField' "date"      "%b %-d, %Y"
   , dateField' "published" "%Y-%m-%dT%H:%M:%SZ"
   , dateField' "updated"   "%Y-%m-%dT%H:%M:%SZ"
+  , tagsField' "tags"      tags
   , defaultContext
   ]
 
@@ -133,8 +156,15 @@ sortRecentFirst' :: MonadMetadata m => [Identifier] -> m [Identifier]
 sortRecentFirst' =
   fmap (fmap itemIdentifier) . recentFirst' . fmap (flip Item ())
 
+tagsField' :: String -> Tags -> Context a
+tagsField' = tagsFieldWith getTags simpleRenderLink (mconcat . intersperse " ")
+
 sortByM :: (Monad m, Ord k) => (a -> m k) -> [a] -> m [a]
 sortByM f = fmap (map fst . sortBy (comparing snd)) . mapM (fmap <$> (,) <*> f)
+
+simpleRenderLink :: String -> (Maybe FilePath) -> Maybe H.Html
+simpleRenderLink tag =
+  fmap (\path -> H.a ! A.href (toValue $ toUrl path) $ toHtml tag)
 
 cmdConfig :: Configuration
 cmdConfig = defaultConfiguration
