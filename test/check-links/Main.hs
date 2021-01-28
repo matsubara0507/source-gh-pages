1ca51f8940a6563c767c7ff3bae72058934686c8
MATSUBARA Nobutada <nobutada0507@gmail.com>
2021-01-28 19:06:32 +0900

    Fix: dead link

--- test/check-links/Main.hs
+++ test/check-links/Main.hs
@@ -33,6 +33,7 @@ main = do
     , "https://www.fun-mooc.fr"   -- 重い
     , "http://www.smlnj.org"      -- 重い
     , "https://www.java-users.jp" -- なんか404になる
+    , "https://twitter.com"       -- 400 になる
     ]
   spec url = it (unpack url) $ linkStatus url `shouldReturn` ok200
 
